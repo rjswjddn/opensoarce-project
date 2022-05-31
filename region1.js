cb4be0961304b6cdaf530eedfe54b7684b787a1d
CE-sw <91242806+Tjdnjs@users.noreply.github.com>
2022-05-31 14:00:20 +0900

    Update region1.js

--- region1.js
+++ region1.js
@@ -5,27 +5,6 @@ let btn2 = document.querySelector("#btn2");
 let btn3 = document.querySelector("#btn3");
 let btn4 = document.querySelector("#btn4");
 let btn5 = document.querySelector("#btn5");
-// let btn6 = document.querySelector("#btn6");
-// let btn7 = document.querySelector("#btn7");
-// let btn8 = document.querySelector("#btn8");
-// let btn9 = document.querySelector("#btn9");
-// let btn10 = document.querySelector("#btn10");
-// let btn11 = document.querySelector("#btn11");
-// let btn12 = document.querySelector("#btn12");
-// let btn13 = document.querySelector("#btn13");
-// let btn14 = document.querySelector("#btn14");
-// let btn15 = document.querySelector("#btn15");
-// let btn16 = document.querySelector("#btn16");
-// let btn17 = document.querySelector("#btn17");
-// let btn18 = document.querySelector("#btn18");
-// let btn19 = document.querySelector("#btn19");
-// let btn20 = document.querySelector("#btn20");
-// let btn21 = document.querySelector("#btn21");
-// let btn22 = document.querySelector("#btn22");
-// let btn23 = document.querySelector("#btn23");
-// let btn24 = document.querySelector("#btn24");
-// let btn25 = document.querySelector("#btn25");
-
 
 btn1.addEventListener('click', () => {
     img.src = "1.png"
