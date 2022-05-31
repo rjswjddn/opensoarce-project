55cb09be809ea345761a0fbd75bf0be04d2cf500
CE-sw <91242806+Tjdnjs@users.noreply.github.com>
2022-05-31 15:04:26 +0900

    Update region1.js

--- region1.js
+++ region1.js
@@ -7,11 +7,17 @@ let btn4 = document.querySelector("#btn4");
 let btn5 = document.querySelector("#btn5");
 
 btn1.addEventListener('click', () => {
-    img.src = "1.png"
+    img.src = "평균거리1.png"
 })
 btn2.addEventListener('click', () => {
-    img.src = "2.png"
+    img.src = "평균거리2.png"
 })
 btn3.addEventListener('click', () => {
-    img.src = "3.png"
+    img.src = "평균거리3.png"
+})
+btn4.addEventListener('click', () => {
+    img.src = "평균거리4.png"
+})
+btn5.addEventListener('click', () => {
+    img.src = "평균거리5.png"
 })
