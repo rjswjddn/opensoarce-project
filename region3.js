c766e5a4c651c3ec229e5f2c89b6d99cba426112
CE-sw <91242806+Tjdnjs@users.noreply.github.com>
2022-05-31 15:29:24 +0900

    Add files via upload

---
+++ region3.js
@@ -0,0 +1,31 @@
+let img = document.querySelector('img');
+
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6");
+let btn7 = document.querySelector("#btn7");
+
+btn1.addEventListener('click', () => {
+    img.src = "대여건수 변화1.png"
+})
+btn2.addEventListener('click', () => {
+    img.src = "대여건수 변화2.png"
+})
+btn3.addEventListener('click', () => {
+    img.src = "대여건수 변화3.png"
+})
+btn4.addEventListener('click', () => {
+    img.src = "대여건수 변화4.png"
+})
+btn5.addEventListener('click', () => {
+    img.src = "대여건수 변화5.png"
+})
+btn6.addEventListener('click', () => {
+    img.src = "대여소개수.png"
+})
+btn7.addEventListener('click', () => {
+    img.src = "거치대개수.png"
+})
